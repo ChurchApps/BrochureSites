5d560bcc4e85e7362b4f49a619cb42ae5af24b8a
Jeremy Zongker <jeremy@zongker.net>
2026-08-16 04:01:08 +0000

    Fix CloudFront querystring object encoding
    Co-authored-by: Jeremy Zongker <jzongker@users.noreply.github.com>

--- Chums.org/cloudfront/redirect-to-b1.js
+++ Chums.org/cloudfront/redirect-to-b1.js
@@ -7,14 +7,27 @@ function handler(event) {
     return request;
   }
 
-  var location = "https://b1.church" + request.uri;
-  if (request.querystring) location += "?" + request.querystring;
+  var loc = 'https://b1.church' + request.uri;
+  var qs = request.querystring;
+  var parts = [];
+  for (var key in qs) {
+    if (!Object.prototype.hasOwnProperty.call(qs, key)) continue;
+    var param = qs[key];
+    if (param.multiValue) {
+      for (var i = 0; i < param.multiValue.length; i++) {
+        parts.push(encodeURIComponent(key) + '=' + encodeURIComponent(param.multiValue[i].value));
+      }
+    } else if (param.value) {
+      parts.push(encodeURIComponent(key) + '=' + encodeURIComponent(param.value));
+    }
+  }
+  if (parts.length) loc += '?' + parts.join('&');
 
   return {
     statusCode: 301,
     statusDescription: "Moved Permanently",
     headers: {
-      location: { value: location },
+      location: { value: loc },
       "cache-control": { value: "max-age=3600" }
     }
   };
